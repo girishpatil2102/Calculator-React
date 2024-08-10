6b1e5c1fbc896470c08aee87be5dfeb4f49896f8
Girish Patil <girishpatil2102@gmail.com>
2024-08-10 11:43:16 +0530

    updtaed button

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,7 +1,7 @@
 import styles from './ButtonsContainer.module.css'
 
 function ButtonsContainer({onButtonClick}){
-   const buttonNames =['C','1','2','+','3','4','-','5','6','*','7','8','/','9','0','=','.','AC'];
+   const buttonNames =['Del','1','2','+','3','4','-','5','6','*','7','8','/','9','0','=','.','AC'];
    return(
       <>
        <div className={styles.buttonContainer}>
